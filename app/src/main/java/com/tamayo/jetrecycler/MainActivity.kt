edf0bf149426fb7c883efeacb6f15def61b6f5b4
Consultant <christopher.tamayocazarez@gmail.com>
2023-03-31 10:27:40 -0400

    Initial commit

---
+++ app/src/main/java/com/tamayo/jetrecycler/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.tamayo.jetrecycler
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tamayo.jetrecycler.ui.theme.JetRecyclerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetRecyclerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
+
